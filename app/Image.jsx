a1ae4b7d9fa491fca81743545c54ed956981a00b
John Ruark <ruark@post.harvard.edu>
2016-05-18 23:22:05 -0400

    Changed carousel to own implementation; removed Slick.

--- app/Image.jsx
+++ app/Image.jsx
@@ -4,23 +4,18 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 export default React.createClass({
   getDate: function() { return new Date(this.props.image.date); },
   mixins: [PureRenderMixin],
-  componentDidMount() {
-    let imageId = this.imageId;
-  },
-  componentWillUnmount() {
-  },
   render: function() {
       this.imageId = this.props.image.id;
-      var divStyle = {
+      const divStyle = {
         width: this.props.maxWidth,
         textAlign: "center"
       }
-      var imgStyle = {
+      const imgStyle = {
         maxWidth: this.props.maxWidth,
         maxHeight: this.props.maxHeight,
         margin: "0 auto"
       };
-      var rotation = this.props.image.rotation || 0;
+      const rotation = this.props.image.rotation || 0;
       return <div className="carousel-itemx">
         <div className="subdiv" style={divStyle}><img style={imgStyle} className={"subimage c"+this.props.image.id} src={"subimage/" + this.props.image.filename + "?" + rotation}></img></div>
         <div className="message-date">{this.getDate().toLocaleTimeString()}</div>
--- app/Images.jsx
+++ app/Images.jsx
@@ -2,47 +2,63 @@ import React from 'react';
 import Image from './Image.jsx';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 const $ = require('jquery');
-const Slider = require("react-slick");
 
 export default React.createClass({
   getInitialState: function() {
-    return { maxWidth: 0, maxHeight: 0 };
+    return { maxWidth: 0, maxHeight: 0, intervalId: 0, currentImgFile: "" };
+  },
+  componentWillUnmount: function() {
+    const iid = this.state.intervalId;
+    if(iid) {
+      window.clearInterval(iid);
+    }
   },
   componentDidMount: function() {
     const _ = this;
+    let intervalId = this.state.intervalId;
+    if( !intervalId ) {
+      intervalId = window.setInterval(function() { _.nextImage(); }, 5000);
+    }
     window.setTimeout(function() {
       const carousel = $(".image-carousel");
       const totalWidth = carousel.width();
       const totalHeight = carousel.height();
       const maxHeight = totalHeight - 100;
       const maxWidth = totalWidth;
-      _.setState({maxWidth: maxWidth, maxHeight: maxHeight});
+      _.setState({maxWidth: maxWidth, maxHeight: maxHeight, intervalId: intervalId});
+      _.nextImage();
     }, 100);
   },
   mixins: [PureRenderMixin],
   getImages: function() {
     return this.props.images || [];
   },
+  nextImage: function() {
+    const images = this.getImages();
+    const currFile = this.state.currentImgFile;
+    let next = 0;
+    if(currFile) {
+      for(let i=0;i<images.length;++i) {
+        if(currFile===images[i].filename) {
+          next=i+1;
+          break;
+        }
+      }
+    }
+    if(next>=images.length) next=0;
+    const nextFile = images.length>0 ? images[next].filename : "";
+    //console.log("nextImage: images.length=" + images.length + ", current=" + currFile + ", next=" + nextFile + ", time=" + Date.now());
+    if(nextFile!==currFile) {
+      this.setState({currentImgFile: nextFile});
+    }
+  },
   render: function() {
-    var settings = {
-      autoplay: true,
-      autoplaySpeed: 4000,
-      centerMode: true,
-      cssEase: 'linear',
-      dots: false,
-      arrows: false,
-      draggable: false,
-      fade: true,
-      infinite: true,
-      speed: 500,
-      swipe: false,
-      touchMove: false,
-      swipeToSlide: false,
-      slickGoTo: 0
-    };
-    
-    return <Slider {...settings} className="image-carousel">{
-          this.getImages().map(a => <div key={a.id} className="carousel-item"><Image maxWidth={this.state.maxWidth} maxHeight={this.state.maxHeight} image={a} /></div>)
-      }</Slider>;
+    const current = this.state.currentImgFile;
+    return <div className="image-carousel">{
+          this.getImages().map((a,index) => 
+            <div key={a.id} className="carousel-item" style={{ display: a.filename===current ? "block" : "none" }}>
+              <Image maxWidth={this.state.maxWidth} maxHeight={this.state.maxHeight} image={a} />
+            </div>)
+      }</div>;
   }
 });
